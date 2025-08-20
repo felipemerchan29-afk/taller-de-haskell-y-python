d49de9f866a24821b04aefe1e5f4c8285429bc27
felipemerchan29-afk <felipemerchan29@gmail.com>
2025-08-20 06:43:36 -0800

    Update problema9.py

--- python/problema9.py
+++ python/problema9.py
@@ -5,10 +5,10 @@ if respuesta == "si":
     print("Ingredientes disponibles: 1)Piña 2) Champiñones")
     opcion = input("Elija un ingrediente: ")
     ingrediente = "Piña" if opcion == "1" else "Champiñones"
-    print("Pizza vegetariana con mozzarella, tomate y", ingrediente)
+    print("Pizza vegetariana con queso, tomate y", ingrediente)
 else:
     print("Ingredientes disponibles: 1) Pepperoni 2) Jamon 3) carnes")
     opcion = input("Elija un ingrediente: ")
     ingredientes = {"1": "Pepperoni", "2": "Jamon", "3": "carnes"}
     ingrediente = ingredientes.get(opcion, "Pepperoni")
-    print("Pizza no vegetariana con mozzarella, tomate y", ingrediente)
+    print("Pizza de", ingrediente)
