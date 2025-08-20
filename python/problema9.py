2f678bc64388e3d7021bb5ccb818486a1ccc25a5
felipemerchan29-afk <felipemerchan29@gmail.com>
2025-08-20 05:36:03 -0800

    Create problema9.py

---
+++ python/problema9.py
@@ -0,0 +1,14 @@
+print("Quiere una pizza vegetariana? (si/no)")
+respuesta = input().lower()
+
+if respuesta == "si":
+    print("Ingredientes disponibles: 1)Piña 2) Champiñones")
+    opcion = input("Elija un ingrediente: ")
+    ingrediente = "Piña" if opcion == "1" else "Champiñones"
+    print("Pizza vegetariana con mozzarella, tomate y", ingrediente)
+else:
+    print("Ingredientes disponibles: 1) Pepperoni 2) Jamon 3) carnes")
+    opcion = input("Elija un ingrediente: ")
+    ingredientes = {"1": "Pepperoni", "2": "Jamon", "3": "carnes"}
+    ingrediente = ingredientes.get(opcion, "Pepperoni")
+    print("Pizza no vegetariana con mozzarella, tomate y", ingrediente)
