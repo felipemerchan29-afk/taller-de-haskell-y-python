e6ccbe09bb1a3e3af5b7ef081903778696d49a09
felipemerchan29-afk <felipemerchan29@gmail.com>
2025-08-20 05:32:13 -0800

    Delete python/python/problema2.py

--- python/python/problema2.py
+++
@@ -1,8 +0,0 @@
-contraseña_guardada = "1234"
-
-entrada = input("Ingrese la contraseña: ")
-
-if entrada.lower() == contraseña_guardada.lower():
-    print("Contraseña correcta")
-else:
-    print("Contraseña incorrecta")
